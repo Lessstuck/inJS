90edb8143fbf93d7cdf9fdb9f990617e19f80eb8
Les Stuck <les@lesstuck.com>
2020-08-10 23:51:36 -0600

    merge with edge cases

--- algo1/merge.js
+++ algo1/merge.js
@@ -1,20 +1,28 @@
-
-let a = [2, 5, 6, 8,];
+// arbitrary test arguments, as ordered arrays
+let a = [2, 5, 6, 7,];
 let b = [1, 3, 4, 9];
+
 lena = a.length;
 lenb = b.length;
 let merged = [];
-let i, j = 0;
+let i = 0;
+let j = 0;
 
-for (let k = 0; k < (lena + lenb); k++ )    {
-    if (a[i] < b[j]) {
+for (let k = 0; k < (lena + lenb); k++) {
+    if (i > lena) {
+        merged[k] = b[j];
+    }
+    else if (j > lenb) {
+        merged[k] = a[i];
+    } else if (a[i] < b[j]) {
         merged[k] = a[i];
         i++;
-    } else {
+    } else if (a[i] > b[j]) {
         merged[k] = b[j];
         j++;
+    } else {
     }
-}
+};    
 
 console.log("merged = ");
 for (let k = 0; k < (lena + lenb); k++) {
