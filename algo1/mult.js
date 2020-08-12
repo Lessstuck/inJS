555d362ed7ad60e5d598900f9b2e9c43f1e0c7e2
Les Stuck <les@lesstuck.com>
2020-08-11 18:34:43 -0600

    get digits from BigInt, first pass

--- algo1/mult.js
+++ algo1/mult.js
@@ -1,5 +1,22 @@
 
 const x = BigInt(process.argv[2]);
 const y = BigInt(process.argv[3]);
+let xs = x.toString();
+const xsl = xs.length;
 
-console.log(x + " * " + y + " = " + (x * y));
+// print string, character by character
+process.stdout.write("stringy:  ");
+for (let i = 0; i < xsl; i++)    {
+    process.stdout.write(xs[i]);
+};
+console.log("");
+
+console.log("string: " + xs);
+
+console.log("Native JS Multiplication: "  + (x * y));
+console.log("Grade School Multiplication: " + (x * y));
+
+function gradeSchoolMult(a, b) {
+    let al = a.length
+
+}
