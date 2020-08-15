03515aef5afe8ed6f208438ed6217ce06bb00b95
Les Stuck <les@lesstuck.com>
2020-08-14 21:09:39 -0600

    non-recursive karatsuba algorithm runs with BigInts :)

--- algo1/mult.js
+++ algo1/mult.js
@@ -2,18 +2,24 @@
 const x = BigInt(process.argv[2]);
 const y = BigInt(process.argv[3]);
 let xs = x.toString();
-let xsl = xs.length;
+let xsl = BigInt(xs.length);
+let xslzeros = (BigInt(10) ** xsl);
+let xslhalfzeros = (BigInt(10) ** (xsl/BigInt(2)));
 
-let a = bigSplit(x);
-let b = bigSplit(y);
+let left = bigSplit(x);
+let right = bigSplit(y);
+let a = BigInt(left[0]);
+let b = BigInt(left[1]);
+let c = BigInt(right[0]);
+let d = BigInt(right[1]);
+let step1 = BigInt(a * c);
+let step2 = BigInt(b * d);
+let step3 = BigInt((a + b) * (c + d));
+let step4 = step3 - step2 - step1;
+let result = (step1 * xslzeros + step2 + step4 * xslhalfzeros);
 
 console.log("Native JS Multiplication: " + (x * y));
-console.log("Karatsuba Multiplication: " + a);
-console.log("Karatsuba Multiplication: " + b);
-
-// function kara(m, n) {
-
-// }
+console.log("Karatsuba Multiplication: " + result);
 
 function bigSplit(number) {
     number = number.toString()
