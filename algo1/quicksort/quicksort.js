eae386c597da195f8377c6e73e6f38f8f1459445
Les Stuck <les@lesstuck.com>
2020-08-30 13:15:56 -0600

    quicksort works - fixed swap call

--- algo1/quicksort/quicksort.js
+++ algo1/quicksort/quicksort.js
@@ -11,7 +11,7 @@ var comparisonCount = 0;  // for comparison count
 
 // read text file of numbers and convert to array of integers
 function readContent(callback) {
-    fs.readFile('./QuickSortSmall.txt', (err, data) => {
+    fs.readFile('./QuickSort.txt', (err, data) => {
         if (err) return callback(err);
         dataString = String(data);
         let re = /\r\n/g;
@@ -28,22 +28,23 @@ function readContent(callback) {
 readContent(function (err, data) {
     len = numberArray.length;
     quickSort(numberArray, 0, len - 1);  // first quickSort call
-    for (let i = 0; i < 8; i++) {
+    for (let i = 9990; i < len; i++) {
         console.log(`array: ${numberArray[i]}`);
     };
-    console.log(`comparisons: ${len}`);
+    console.log(`comparisons: ${comparisonCount}`);
 });
 
 function partition(A, l, r) {
-    let p = A[l];
+    let p = A[r];
     let i = l + 1;
     for (let j = l + 1; j <= r; j++) {
         if (A[j] < p) {
-            swap(A[i], A[j]);
+            comparisonCount++;
+            swap(A, i, j);
             i++; 
         }
     }
-    swap(A[l], A[i - 1]);
+    swap(A, l, i - 1);
     return (i - 1);
 }
 
@@ -52,16 +53,16 @@ function quickSort(A, l, r) {
         return;
     }
     i = choosePivot(A, l, r)
-    swap(A[l], A[i]);
+    swap(A, i, l);
     let j = partition(A, l, r);
     quickSort(A, l, j - 1);
     quickSort(A, j + 1, r);
 }
 
-function swap(X, Y) {
-    let temp = X;
-    X = Y;
-    Y = temp;
+function swap(Z, x, y) {
+    let temp = Z[x];
+    Z[x] = Z[y];
+    Z[y] = temp;
 }
 
 function choosePivot(A, l, r) {
