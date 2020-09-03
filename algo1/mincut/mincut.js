a0ac58377dd059cbab895c304402dce93c2c389f
Les Stuck <les@lesstuck.com>
2020-09-03 16:04:13 -0600

    successfully unpacks text file into 2-dimensional array

--- algo1/mincut/mincut.js
+++ algo1/mincut/mincut.js
@@ -7,15 +7,19 @@ let cr = /\r/g;
 let nl = /\n/g;
 let tb = /\t/g;
 let fileArray = [];
-// let fileStringLine = [];
+let fileArrayThingy = [];
+let fileArrayNumber = [];
+let arrayOfArrays = [];
 
 let fileStringLines = fileString.split(cr);
-// console.log(`${fileStringLines}`);
-for (let i = 0; i < 3; i++)    {
-    console.log(`${fileStringLines[i]}`);
-    let fileStringSplit = fileStringLines[i].split[tb];
-    console.log(`${fileStringSplit}`);
-    // for (let j = 0; j < 10; j++) {
-    //     fileArray[i][j] = Number(fileStringSplit[j]); 
-    // };
+for (let i = 2; i >= 0; i--)    {
+    let fileStringLine = fileStringLines[i];
+    fileArray = fileStringLine.split(tb);
+    for (let j = 0; j < 10; j++) {
+        fileArrayNumber[j] = Number(fileArray[j]);
+    };
+    arrayOfArrays.push(fileArrayNumber);
 };
+
+console.log(`${arrayOfArrays[0][3]}`);
+
