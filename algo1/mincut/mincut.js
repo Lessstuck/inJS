c3051c86f4b7687d1cb29039b3f3284dc36ca252
Les Stuck <les@lesstuck.com>
2020-09-04 16:50:05 -0600

    finally constructs 2D array from text file

--- algo1/mincut/mincut.js
+++ algo1/mincut/mincut.js
@@ -2,23 +2,36 @@ var fs = require("fs");
 
 var fileText = fs.readFileSync('./kargerMinCut.txt');
 
-fileString = String(fileText);
+let fileString = String(fileText);
 let cr = /\r/g;
 let nl = /\n/g;
 let tb = /\t/g;
+let sp = /\s+/g
 let fileArray = [];
 let fileArrayThingy = [];
 let fileNumberArray = [];
-let arrayOfArrays = [];
+let arrayOfArrays = [[]];
 
 let fileStringLines = fileString.split(cr); // 
 for (let i = 0; i < 4; i++)    {  
-    fileStringArray = fileStringLines[i].split(tb);
-    fileStringArray.pop(); // delete tab at end of line
-    // for (let j = 0; j < fileStringArray.length; j++) {
+    fileStringArray = fileStringLines[i].split(sp);
+    fileStringArray.pop(); // delete tab at end
+    if (i > 0) {
+        fileStringArray.shift(); // delete newline at beginning
+    }
+    console.log(`${fileStringArray}`);
+    fileNumberArray = []
     for (let j = 0; j < fileStringArray.length; j++) {
         fileNumberArray[j] = Number(fileStringArray[j]);
-    } 
-    arrayOfArrays.push(fileNumberArray);
-    console.log(`Length: ${arrayOfArrays.length}; Content:  ${arrayOfArrays[0]}`);
+    }
+    arrayOfArrays[i] = fileNumberArray;
 };
+
+for (let m = 0; m < 4; m++) {
+    var innerArrayLength = arrayOfArrays[m].length;
+    for (let n = 0; n < 4; n++)  {
+        console.log(`Length: ${innerArrayLength}; Content:  ${Number(arrayOfArrays[m][n])}`);
+    }
+    
+    
+}
