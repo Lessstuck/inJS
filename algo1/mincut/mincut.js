06f56745f067136ad0f0a9159085d6f8d49d1180
Les Stuck <les@lesstuck.com>
2020-09-06 23:41:32 -0600

    first version of contract

--- algo1/mincut/mincut.js
+++ algo1/mincut/mincut.js
@@ -1,6 +1,5 @@
 var fs = require("fs");
 
-
 let cr = /\r/g;
 let nl = /\n/g;
 let tb = /\t/g;
@@ -8,7 +7,7 @@ let sp = /\s+/g
 let fileArray = [];
 let fileArrayThingy = [];
 let fileNumberArray = [];
-let arrayOfArrays = [[]];
+var arrayOfArrays = [[]];
 
 // read an format input, from text to array of arrays of integers
 var fileText = fs.readFileSync('./kargerMinCut.txt');
@@ -27,10 +26,37 @@ for (let i = 0; i < fileStringLines.length; i++)    {
     arrayOfArrays[i] = fileNumberArray;
 };
 
-// random contraction 
-let chosenEdge = randomEdge(arrayOfArrays);
-merged = merge(chosenEdge);
-console.log(`chosenEdge: ${chosenEdge[0]} ${chosenEdge[1]} merged: ${merged}`);
+// rContract(arrayOfArrays);
+console.log(`result ${arrayOfArrays[0]} ${arrayOfArrays.length} `);
+
+// function rContract(arrayOfArrays) {
+    if (arrayOfArrays.length < 2) {
+        return arrayOfArrays;
+    }
+    let chosenEdge = randomEdge(arrayOfArrays);
+    var v1 = chosenEdge[0];
+    var v2 = chosenEdge[1];
+    merged = merge(chosenEdge);
+    arrayOfArrays[chosenEdge[0]] = merged; // merge replaces tail
+    arrayOfArrays.splice(chosenEdge[1], 1);
+    // console.log(`chosenEdge: ${chosenEdge[0]} ${chosenEdge[1]} merged: ${merged}`);
+    // renumber after splice
+    if (arrayOfArrays.length > 2) {
+        for (let i = 2; i < arrayOfArrays.length; i++) {
+            var innerArrayLength = arrayOfArrays[i].length;
+            for (let j = 1; j < innerArrayLength - 1; j++) {
+                if (arrayOfArrays[i][j] == arrayOfArrays[v2][0]) {  // match first value (vertex number) of head array
+                    arrayOfArrays[i][j] = arrayOfArrays[v1][0];   // replace second vertex with first vertex
+                } else if (arrayOfArrays[i][j] > arrayOfArrays[v2][0]) {   // shifted vertex numbers reduced by one
+                    arrayOfArrays[i][j] = arrayOfArrays[i][j] - 1;
+
+                }
+            }
+        }
+
+    }
+//     rContract(arrayOfArrays);
+// }
 
 
 function randomEdge(arrayOfArrays) {
@@ -42,26 +68,26 @@ function randomEdge(arrayOfArrays) {
     var innerArrayLength = arrayOfArrays[outerArrayChoice - 1].length; 
     var innerArrayPosition = Math.floor(Math.random() * innerArrayLength - 1) + 1;  // choosing from all except first
     var innerArrayChoice = innerArray[innerArrayPosition];
-    var chosenEdge = [outerArrayChoice, innerArrayChoice];
+    var chosenEdge = [outerArrayChoice, innerArrayChoice]; // indices
     return (chosenEdge);
 }
 
 function merge(chosenEdge) {
-    let v1 = chosenEdge[0];
+    let v1 = chosenEdge[0]; // index
     let tail = arrayOfArrays[v1 - 1];   // edges are counted from 1, but indices from 0
     let v2 = chosenEdge[1];
     let head = arrayOfArrays[v2 - 1];   // edges are counted from 1, but indices from 0
-
-    // remve v2 from tail to avoid self-loop
-    let index = tail.indexOf(v2);
+    // remve v2 vertex number from tail, v1 vertex number from head to delete chosen edge
+    let index;
+    index = tail.indexOf(head[0]);
     tail.splice(index, 1);
-
+    index = head.indexOf(tail[0]);
+    head.splice(index, 1);
+    // assemble new vertex at end of array of arrays
     arrayOfArrays.push(tail);
     let merged = arrayOfArrays[arrayOfArrays.length - 1];
-
     for (let i = 1; i < head.length; i++) {
-        // console.log(`head loop: ${head[i]}`);
-
+        merged.push(head[i]);
     }
     return merged;
 }
