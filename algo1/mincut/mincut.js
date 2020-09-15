bfcb6cb9e375d3d01006a2385b32bde4106fcc32
Les Stuck <les@lesstuck.com>
2020-09-15 00:49:53 -0600

    small test text, tracking down index error

--- algo1/mincut/mincut.js
+++ algo1/mincut/mincut.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 
-let cr = /\r/g;
+let cr = /\r|\n/g;
 let nl = /\n/g;
 let tb = /\t/g;
 let sp = /\s+/g
@@ -10,7 +10,8 @@ let fileNumberArray = [];
 var arrayOfArrays = new Array;
 
 // read an format input, from text to array of arrays of integers
-var fileText = fs.readFileSync('./kargerMinCut.txt');
+// var fileText = fs.readFileSync('./kargerMinCut.txt');
+var fileText = fs.readFileSync('./kargerSmall.txt');
 let fileString = String(fileText);
 let fileStringLines = fileString.split(cr); // 
 for (let i = 0; i < fileStringLines.length; i++)    {  
@@ -21,52 +22,56 @@ for (let i = 0; i < fileStringLines.length; i++)    {
     }
     fileNumberArray = []
     for (let j = 0; j < fileStringArray.length; j++) {
-        fileNumberArray[j] = Number(fileStringArray[j]);
+        fileNumberArray[j] = Number(fileStringArray[j]); // convert to array of numbers
     }
     arrayOfArrays[i] = fileNumberArray;
 };
-
+console.log(`fileStringArray: ${arrayOfArrays}`)
 rContract(arrayOfArrays);
-
+console.log(`arrayOfArrays.length: ${arrayOfArrays.length}`)
 
 function rContract(arrayOfArrays) {
-    if (arrayOfArrays.length < 2) {
-        return arrayOfArrays;
-    }
-    let chosenEdge = randomEdge(arrayOfArrays);
-    var v1 = chosenEdge[0];
-    var v2 = chosenEdge[1];
-    merged = merge(chosenEdge);
-    arrayOfArrays[chosenEdge[0]] = merged; // merge replaces tail
-    arrayOfArrays.splice(chosenEdge[1], 1); // delete head
-    arrayOfArrays.pop(); // delete merged array
-    // renumber after splice
-    if (arrayOfArrays.length > 2) {
-        for (let i = 2; i < arrayOfArrays.length; i++) {
-            var innerArrayLength = arrayOfArrays[i].length;
-            for (let j = 1; j < innerArrayLength - 1; j++) {
-                if (arrayOfArrays[i][j] == arrayOfArrays[v2][0]) {  // match first value (vertex number) of head array
-                    arrayOfArrays[i][j] = arrayOfArrays[v1][0];   // replace second vertex with first vertex
-                } else if (arrayOfArrays[i][j] > arrayOfArrays[v2][0]) {   // shifted vertex numbers reduced by one
-                    arrayOfArrays[i][j] = arrayOfArrays[i][j] - 1;
 
+    if (arrayOfArrays.length <= 1) {
+        return arrayOfArrays;
+    } else {
+
+        let chosenEdge = randomEdge(arrayOfArrays);
+        var v1 = chosenEdge[0];
+        var v2 = chosenEdge[1];
+        merged = merge(chosenEdge);
+        arrayOfArrays[chosenEdge[0]] = merged; // merge replaces tail
+        arrayOfArrays.splice(chosenEdge[1], 1); // delete head
+        arrayOfArrays.pop(); // delete merged array
+        // renumber after splice
+        if (arrayOfArrays.length > 2) {
+            for (let i = 2; i < arrayOfArrays.length; i++) {
+                var innerArrayLength = arrayOfArrays[i].length;
+                for (let j = 1; j < innerArrayLength - 1; j++) {
+                    if (arrayOfArrays[i][j] == arrayOfArrays[v2][0]) {  // match first value (vertex number) of head array
+                        arrayOfArrays[i][j] = arrayOfArrays[v1][0];   // replace second vertex with first vertex
+                    } else if (arrayOfArrays[i][j] > arrayOfArrays[v2][0]) {   // shifted vertex numbers reduced by one
+                        arrayOfArrays[i][j] = arrayOfArrays[i][j] - 1;
+
+                    }
                 }
             }
-        }
 
+        }
+        rContract(arrayOfArrays);
     }
-    rContract(arrayOfArrays);
+
 }
 console.log(`result ${arrayOfArrays[0]} ${arrayOfArrays.length} `);
 
 function randomEdge(arrayOfArrays) {
     // choose inner array = [0] vertex
     var outerArrayLength = arrayOfArrays.length;
-    var outerArrayChoice = Math.floor(Math.random() * outerArrayLength - 1);
-    let innerArray = arrayOfArrays[outerArrayChoice - 1];
+    var outerArrayChoice = Math.floor(Math.random() * outerArrayLength);
+    let innerArray = arrayOfArrays[outerArrayChoice];
     // choose element of inner array [i] = vertices connected by one edge
-    var innerArrayLength = arrayOfArrays[outerArrayChoice - 1].length; 
-    var innerArrayPosition = Math.floor(Math.random() * innerArrayLength - 1) + 1;  // choosing from all except first
+    var innerArrayLength = arrayOfArrays[outerArrayChoice].length; 
+    var innerArrayPosition = Math.floor(Math.random() * innerArrayLength) + 1;  // choosing from all except first
     var innerArrayChoice = innerArray[innerArrayPosition];
     var chosenEdge = [outerArrayChoice, innerArrayChoice]; // indices
     return (chosenEdge);
