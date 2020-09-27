31a9af5a3b000ea085b03c450929c254e6c413d7
Les Stuck <les@lesstuck.com>
2020-09-27 09:54:39 -0600

    linked list function runs without errors

--- eloquentJS/04/reverseArrayInPlace.js
+++ eloquentJS/04/reverseArrayInPlace.js
@@ -1,11 +1,11 @@
 let [, , ...args] = process.argv;
 let argslen = args.length;
-// console.log(`argslen - 1: ${argslen - 1}`);
-console.log(`args: ${args}`);
-for (let i = 0; i < (argslen/2); i++)   {
-    swap(args[i], args[argslen - i - 1]);
+for (let i = 0; i < (argslen / 2); i++)   {
+    let swapped = swap(args[i], args[argslen - i - 1]);
+    args[i] = swapped[0];
+    args[argslen - i - 1] = swapped[1];
 }
-console.log(`args: ${args}`);
+console.log(`${args}`);
 
 function swap(a, b) {
     let stash = a;
