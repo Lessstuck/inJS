df2f310947783202ccaefc6d84f029fbfbe383e6
Les Stuck <les@lesstuck.com>
2020-10-04 23:43:13 -0600

    fixed list head data in class def

--- eloquentJS/04/arrayToList.js
+++ eloquentJS/04/arrayToList.js
@@ -18,11 +18,14 @@ class LinkedList  {
         }
         return count;
     }
+    getFirst() {
+        return this.head;
+    }
 };
    
 let node = [];
 function arrayToList(arr) {
-    node[0] = new ListNode;
+    node[0] = new ListNode(arr[0]);
     for (let i = 1; i < arr.length; i++)  {
         node[i] = new ListNode(arr[i]);
         node[i - 1].next = node[i];
@@ -32,18 +35,18 @@ function arrayToList(arr) {
 };
 
 let arr = [2, 3, 5];
-console.log(arrayToList(arr).head.next.data); 
+console.log(arrayToList(arr).head.data); 
 
-function listToArray(list)  {
+function listToArray(list) {
     let arr = [];
-//     let delist = list.head;
-//     let i = 0;
-//     while (delist.next) {
-//         arr.push(delist.data);
-//         // i++;
-//     }
+    node = list.head;
+    arr.push(node.data);
+    while (node.next) {
+        arr.push(node.data);
+        node = node.next;
+    }
     return arr;
 }
 
-console.log(arrayToList([2, 4, 5, 7]).size());
+// console.log(arrayToList([2, 4, 5, 7]).size());
 console.log(listToArray(arrayToList([2, 3, 5])));
