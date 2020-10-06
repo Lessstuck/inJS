11b11802707a85faff25902b51f77838a449f48a
Les Stuck <les@lesstuck.com>
2020-10-05 23:41:53 -0600

    trivial cases dispensed with

--- eloquentJS/04/deepEqual.js
+++ eloquentJS/04/deepEqual.js
@@ -1,6 +1,12 @@
 
 function deepEqual(x, y) {
-    return (x == y);
+    if (x === null) return "x is null"
+    else if (y === null) return "y is null"
+    else if (typeof (x) != "object" && typeof (y) != "object") {
+        return (x == y);
+    } else {
+        return (x == y);
+    }
 }
 
 console.log(deepEqual(3, 4));
