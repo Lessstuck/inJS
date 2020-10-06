65531f4f4855b477cee7c9fe4f1676d4a442f068
Les Stuck <les@lesstuck.com>
2020-10-06 00:02:29 -0600

    deepEqual compares keys of objects or values of other things, seems to work

--- eloquentJS/04/deepEqual.js
+++ eloquentJS/04/deepEqual.js
@@ -5,9 +5,25 @@ function deepEqual(x, y) {
     else if (typeof (x) != "object" && typeof (y) != "object") {
         return (x == y);
     } else {
-        return (x == y);
+        const xKeyz = Object.keys(x);
+        const yKeyz = Object.keys(y);
+        for (let i = 0; i < xKeyz.length; i++)  {
+            if (xKeyz[i] != yKeyz[i]) return false;
+        }
+        for (let i = 0; i < yKeyz.length; i++) {
+            if (xKeyz[i] != yKeyz[i]) return false;
+        }
+        return true;
     }
 }
 
+const objectA = { foo: 3, bar: 2 };
+const objectB = { foo: 2, bar: 1 };
+const objectC = { foo: 12 };
+const objectD = { foo: 3, bar: 2, bam: 0 };
+
 console.log(deepEqual(3, 4));
 console.log(deepEqual(3, 3));
+console.log(deepEqual(objectA, objectB));
+console.log(deepEqual(objectA, objectC));
+console.log(deepEqual(objectA, objectD));
