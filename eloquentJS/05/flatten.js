4c6492fd7bebee4445fb5119957624d931469a17
Les Stuck <les@lesstuck.com>
2020-10-08 00:03:10 -0600

    cleaned up and added comment

--- eloquentJS/05/flatten.js
+++ eloquentJS/05/flatten.js
@@ -2,10 +2,5 @@ var arrayOfArrays = [[1, 2, 3], [4, 5], [4, 3, 2, 1]];
 
 const reducer = (accumulator, currentValue) => accumulator.concat(currentValue);
 
-function arrayFlatten(arrayOfArrays)  {
-        let result = arrayOfArrays.reduce(reducer);
-        return result;
-};
-
-let flatArray = arrayFlatten(arrayOfArrays);
-console.log(flatArray);
+console.log(arrayOfArrays.reduce(reducer));
+// [1, 2, 3, 4, 5, 4, 3, 2, 1]
