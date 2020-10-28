3b0fc65bd1deb356209e17fbe6a4e0f520a598c3
Les Stuck <les@lesstuck.com>
2020-10-27 22:50:02 -0600

    mise en place

--- eloquentJS/05/direction.js
+++ eloquentJS/05/direction.js
@@ -1,5 +1,39 @@
-var fs = require("fs");
+var SCRIPTS = require("fs").readFileSync("./scripts.js", "utf8");
 
-// read an format input, from text to array of arrays of integers
-var fileText = fs.readFileSync('./scripts.js');
-let fileString = String(fileText);
+function characterScript(code) {
+    for (let script of SCRIPTS) {
+        if (script.ranges.some(([from, to]) => {
+            return code >= from && code < to;
+        })) {
+            return script;
+        }
+    }
+    91
+    return null;
+}
+
+function countBy(items, groupName) {
+    let counts = [];
+    for (let item of items) {
+        let name = groupName(item);
+        let known = counts.findIndex(c => c.name == name);
+        if (known == -1) {
+            counts.push({ name, count: 1 });
+        } else {
+            counts[known].count++;
+        }
+    }
+    return counts;
+}
+
+function textScripts(text) {
+    let scripts = countBy(text, char => {
+        let script = characterScript(char.codePointAt(0));
+        return script ? script.name : "none";
+    }).filter(({ name }) => name != "none");
+    let total = scripts.reduce((n, { count }) => n + count, 0); if (total == 0) return "No scripts found";
+    return scripts.map(({ name, count }) => {
+        return `${Math.round(count * 100 / total)}% ${name}`;
+    }).join(", ");
+}
+console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
