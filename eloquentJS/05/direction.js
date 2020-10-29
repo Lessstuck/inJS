0f657ff9a49ce3a11180756b9b086a5c780cf950
Les Stuck <les@lesstuck.com>
2020-10-29 15:35:51 -0600

    maximum script direction detector works

--- eloquentJS/05/direction.js
+++ eloquentJS/05/direction.js
@@ -1,8 +1,6 @@
 // using modified kludge to expose SCRIPTS
 var loader = require("./load.js");
 loader("scripts.js");
-console.log(SCRIPTS);
-
 
 function characterScript(code) {
     for (let script of SCRIPTS) {
@@ -16,28 +14,29 @@ function characterScript(code) {
     return null;
 }
 
-// function countBy(items, groupName) {
-//     let counts = [];
-//     for (let item of items) {
-//         let name = groupName(item);
-//         let known = counts.findIndex(c => c.name == name);
-//         if (known == -1) {
-//             counts.push({ name, count: 1 });
-//         } else {
-//             counts[known].count++;
-//         }
-//     }
-//     return counts;
-// }
+function countBy(items, groupName) {
+    let counts = [];
+    for (let item of items) {
+        let name = groupName(item);
+        let known = counts.findIndex(c => c.name == name);
+        if (known == -1) {
+            counts.push({ name, count: 1 });
+        } else {
+            counts[known].count++;
+        }
+    }
+    return counts;
+}
 
-// function textScripts(text) {
-//     let scripts = countBy(text, char => {
-//         let script = characterScript(char.codePointAt(0));
-//         return script ? script.name : "none";
-//     }).filter(({ name }) => name != "none");
-//     let total = scripts.reduce((n, { count }) => n + count, 0); if (total == 0) return "No scripts found";
-//     return scripts.map(({ name, count }) => {
-//         return `${Math.round(count * 100 / total)}% ${name}`;
-//     }).join(", ");
-// }
-// console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
+function textScripts(text) {
+    let scripts = countBy(text, char => {
+        let script = characterScript(char.codePointAt(0));
+        return script ? script.direction : "none";
+    }).filter(({ name }) => name != "none");
+    return scripts.reduce((a, b) => {
+        return a.count < b.count ? b.name : a.name;
+    });
+}
+
+// test
+console.log(textScripts('Hey, مساء الخير'));
