95778b4edc79731edcaaa86bbc740017c9b74707
Les Stuck <les@lesstuck.com>
2020-11-03 15:37:01 -0700

    vector.js works

--- eloquentJS/05/06/vector.js
+++ eloquentJS/05/06/vector.js
@@ -9,10 +9,14 @@ class Vec {
     minus(j, k)   {
         return new Vec(this.x - j, this.y - k);
     }
+    length() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
 }
 
 let vec = new Vec(3, 4);
 sum = vec.plus(6, 8);
 dif = vec.minus(1, 1);
-console.log("sum: " + sum);
-console.log("dif: " + dif);
+console.log("sum: " + sum.x + ", " + sum.y);
+console.log("dif: " + dif.x + ", " + dif.y);
+console.log("length: " + vec.length());
