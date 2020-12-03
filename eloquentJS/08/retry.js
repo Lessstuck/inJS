e38279e59cec572c4e61124e9a72dbd91247a47c
Les Stuck <les@lesstuck.com>
2020-12-02 22:13:28 -0700

    Clean up and add comments

--- eloquentJS/08/retry.js
+++ eloquentJS/08/retry.js
@@ -1,18 +1,17 @@
+// primitiveMultiply succeeds 20% of the time
 function primitiveMultiply(x, y) {
     let rando = Math.random();
     if (rando < .2) {
         return (x * y);
     }
-    // return ("rando");
     throw new Error("MultiplicationFailure: " + rando);
 };
 
+// catchs error to retry primitiveMultiply
 function retry(x, y) {
     let retried;
     try {
         return primitiveMultiply(3, 5);
-        // retried = primitiveMultiply(3, 5);
-        // console.log(retried);
     } catch (error) {
         return retry(x, y);
     }
