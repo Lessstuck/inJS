19c6187ddd30b57969fdd04456618931c6f8c5e6
Les Stuck <les@lesstuck.com>
2020-12-02 21:29:49 -0700

    Add aleatoric conditional

--- eloquentJS/08/retry.js
+++ eloquentJS/08/retry.js
@@ -1,5 +1,9 @@
 function primitiveMultiply(x, y) {
-    return (x * y);
+    let rando = Math.random();
+    if (rando < .2) {
+        return (x * y);
+    }
+    return ("rando");
 };
 let retried = primitiveMultiply(3, 5)
 console.log(retried);
