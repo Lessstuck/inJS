edada0cdd319356ea9296bfafcf9ace0670ba185
Les Stuck <les@lesstuck.com>
2020-12-06 14:00:10 -0700

    Add finally to ensure box is locked

--- eloquentJS/08/box.js
+++ eloquentJS/08/box.js
@@ -4,25 +4,43 @@ const box = {
     lock() { this.locked = true; },
     _content: [],
     get content() {
-        if (this.locked) throw new Error("Locked!");
-        return this._content;
+        if (this.locked) {
+            throw new Error("Locked!");
+        } else {
+            return this._content;
+        }
     }
 };
 
+// my code
 function withBoxUnlocked(body) {
-    // Your code here.
+    try {
+        if (Math.random() < .5) {
+            box.unlock();
+        } else {
+            box.lock();
+        }
+        console.log("box locked?: " + box.locked);
+        body();
+        box.lock();
+    } catch (e) {
+            console.log("Sorry, the box is locked.");
+    } finally {
+        box.lock();
+    }
 }
 
 withBoxUnlocked(function () {
     box.content.push("gold piece");
 });
 
-try {
-    withBoxUnlocked(function () {
-        throw new Error("Pirates on the horizon! Abort!");
-    });
-} catch (e) {
-    console.log("Error raised: " + e);
-}
+// try {
+//     withBoxUnlocked(function () {
+//         throw new Error("Pirates on the horizon! Abort!");
+//     });
+// } catch (e) {
+//     console.log("Error raised: " + e);
+// }
+
 console.log(box.locked);
 // → true
