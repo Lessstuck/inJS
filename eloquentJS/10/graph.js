790bf2ab569205467d8d634026ac435bad3e799a
Les Stuck <les@lesstuck.com>
2020-12-17 22:39:37 -0700

    Fix addEdge function in graph.js when running roads.js

--- eloquentJS/10/graph.js
+++ eloquentJS/10/graph.js
@@ -1,16 +1,16 @@
 
 exports.buildGraph = function(edges) {
-  // let graph = Object.create(null);
   let graph = [];
   function addEdge(from, to) {
-    if (graph[from] == null) {
-      graph[from] = [to];
+    if (graph[0] == null) {
+      graph[0] = [from, to];
     } else {
-      graph[from].push(to);
+      graph.push([from, to]);
     }
   }
   for (let i = 0; i < edges.length; i++) {
-      addEdge(edges[i][0], edges[i][1]);
+    addEdge(edges[i][0], edges[i][1]);
+    addEdge(edges[i][1], edges[i][0]);
   }
   return graph;
 }
