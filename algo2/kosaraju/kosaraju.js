49889707521d6c68ef0a664dc2b03d08ab06e7c1
Les Stuck <les@lesstuck.com>
2021-03-21 15:33:47 -0600

    Clean up

--- algo2/kosaraju/kosaraju.js
+++ algo2/kosaraju/kosaraju.js
@@ -17,10 +17,7 @@ let fileString = String(fileText);
 let fileStringLines = fileString.split(cr); // 
 for (let i = 0; i < fileStringLines.length; i++)    {  
     fileStringArray = fileStringLines[i].split(sp);
-    fileStringArray.pop(); // delete tab at end     // restore for full txt file!!
-    // if (i > 0) {                                     // restore for full txt file!!
-    //     fileStringArray.shift(); // delete newline at beginning
-    // }
+    fileStringArray.pop();
     fileNumberArray = []
     for (let j = 0; j < fileStringArray.length; j++) {
         fileNumberArray[j] = Number(fileStringArray[j]); // convert to array of numbers
@@ -30,4 +27,3 @@ for (let i = 0; i < fileStringLines.length; i++)    {
 
 console.log(originalArrayOfArrays)
 console.log(originalArrayOfArrays[0][0] + originalArrayOfArrays[1][1])
-// console.log(fileStringLines)
