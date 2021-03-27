3c21bab6f6cd8a98b67dbbcd7a17ab4a6de4ade7
Les Stuck <les@lesstuck.com>
2021-03-26 23:02:15 -0600

    Counts back, but doesn't continue forEach

--- algo2/kosaraju/depthFirstSearch.js
+++ algo2/kosaraju/depthFirstSearch.js
@@ -20,6 +20,9 @@ let finishingTimes = new Array;
 for (let i = 0; i < maxVertex; i++) {
     finishingTimes[i] = 0;
 }
+
+let goingBack = false;
+
 let leader = 0;
 let leaders = new Array;
 for (let i = 0; i < maxVertex; i++) {
@@ -28,6 +31,7 @@ for (let i = 0; i < maxVertex; i++) {
 
 // start at highest numbered vertex for kosaraju algorithm
 for (let i = maxVertex; i > 0; i--) {
+    goingBack = false;
     previousVertices = [];
     leaders[i] = i;
     DFS(adjacencyList, i);
@@ -37,9 +41,16 @@ var connectedNodes = new Array;
 function DFS(adjacencyList, startVertex) {
     let startVertexIndex = startVertex - 1;
     console.log("\n")
-    console.log(`startVertex: ${startVertex}`);
-    if (visitedVertices[startVertexIndex] == 1) {   // going back means calling DFS on visted vertex
-        leaders[startVertex]
+    console.log(`DFS begin startVertex: ${startVertex}`);
+    if (goingBack == true) {   // going back means calling DFS on visted vertex
+        finishingTime++;
+        finishingTimes[startVertex - 1] = finishingTime;
+        console.log("going back finishingTimes: " + finishingTimes + "\n\n");
+        if (previousVertices.length != 0) {   // set of of previous vertices must not be empty
+            DFS(adjacencyList, previousVertices.pop());
+        }
+
+        leaders[startVertex] = 0; // fix this when coding DFS #2
     }
     visitedVertices[startVertexIndex] = 1;  // set this vertex to "visited"
     connectedNodes = [...adjacencyList[startVertexIndex]];
@@ -49,15 +60,16 @@ function DFS(adjacencyList, startVertex) {
             console.log("finishingTime: " + finishingTime);
             previousVertices.push(element);
             DFS(adjacencyList, element);
-        } else {   // if visited, go back
-            finishingTime++;
-            console.log("finishingTime: " + finishingTime);
-            finishingTimes[startVertex - 1] = finishingTime;
-            console.log("startVertexIndex: " + startVertexIndex + "\n " + "finishingTimes: " + finishingTimes + "\n\n");
-            if (previousVertices.length != 0) {   // must have positive length
-                finishingTime++;
-                DFS(adjacencyList, previousVertices.pop());  
-            }   
         }
     });
+    // if no more unvisited vertices, go back
+    goingBack = true;
+    finishingTime++;
+    finishingTimes[startVertex - 1] = finishingTime;
+    console.log("end-of-the-line finishingTime: " + finishingTime);
+    console.log("startVertexIndex: " + startVertexIndex + "\n " + "finishingTimes: " + finishingTimes + "\n\n");
+    previousVertices.pop();
+    if (previousVertices.length != 0) {   // set of of previous vertices must not be empty
+        DFS(adjacencyList, previousVertices.pop());
+    }
 }
