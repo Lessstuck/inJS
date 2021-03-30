436b9893b21a70d3145b83bb328322c09e9d586c
Les Stuck <les@lesstuck.com>
2021-03-30 10:47:14 -0600

    Merge DFS into kosaraju.js

--- algo2/kosaraju/kosaraju.js
+++ algo2/kosaraju/kosaraju.js
@@ -49,7 +49,59 @@ for (let i = 0; i < originalLength; i++) {
         adjacencyListRev[inputVertex - 1].push(inputEdgeArray[i][1]);  // add element to array
     }
 }
-console.log("adjacencyList: ");
-console.log(adjacencyList)
-console.log("adjacencyListRev: ");
-console.log(adjacencyListRev)
+// Depth first search of reversed graph
+// Assuming that the vertex numbers are natural numbers,
+// increasing, with none skipped,
+maxVertex = adjacencyListRev.length;
+let visitedVertices = new Array;
+for (let i = 0; i < maxVertex; i++) {
+    visitedVertices[i] = 0;
+}
+let previousVertices = new Array;
+
+let finishingTime = 0;
+let finishingTimes = new Array;
+for (let i = 0; i < maxVertex; i++) {
+    previousVertices[0] = i;
+    finishingTimes[i] = 0;
+}
+
+let goingBack = false;
+
+let leader = 0;
+let leaders = new Array;
+for (let i = 0; i < maxVertex; i++) {
+    leaders[i] = 0;
+}
+
+// start at highest numbered vertex for kosaraju algorithm
+for (let i = maxVertex; i > 0; i--) {
+    if (visitedVertices[i - 1] == 1) {
+        continue;
+    }
+    goingBack = false;
+    previousVertices[0] = i;
+    leaders[i] = i;
+    DFS(adjacencyListRev, i);
+}
+console.log(finishingTimes);
+
+var connectedNodes = new Array;
+function DFS(adjacencyListRev, startVertex) {
+    let startVertexIndex = startVertex - 1;
+    visitedVertices[startVertexIndex] = 1;  // set this vertex to "visited"
+    connectedNodes = [...adjacencyListRev[startVertexIndex]];
+    connectedNodes.shift(); // remove the first vertex 
+    connectedNodes.forEach(element => {
+        if (visitedVertices[element - 1] == 0) {   // if unvisited, recurse, going deeper
+            goingBack = false;
+            previousVertices.push(element);
+            DFS(adjacencyListRev, element);
+        }
+    });
+    // if no more unvisited vertices, go back
+    finishingTime++;
+    finishingTimes[finishingTime - 1] = startVertex;
+    previousVertices.pop();
+    let previousLength = previousVertices.length;
+}
