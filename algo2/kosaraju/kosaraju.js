94283441f2763eeecf4bf0242ab1a5d57918a977
Les Stuck <les@lesstuck.com>
2021-04-03 13:06:07 -0600

    Constructs adjacency list of remapped edge list

--- algo2/kosaraju/kosaraju.js
+++ algo2/kosaraju/kosaraju.js
@@ -4,29 +4,29 @@ let cr = /\r|\n/g;
 let sp = /\s|\t/g;
 let fileNumberArray = new Array;
 let inputEdgeArray = new Array;
+let inputEdgeArrayRev = new Array;
 let fileStringLines = new Array;
 
 // read text file, convert to array of arrays of integers
-var fileText = fs.readFileSync('./kosarajuGraphSmall.txt');
+var fileText = fs.readFileSync('kosarajuGraphSmall.txt');
 let fileString = String(fileText);
 fileStringLines = fileString.split(cr);
-console.log(`fileStringLines: ${fileStringLines}`)
 for (let i = 0; i < fileStringLines.length; i++)    {  
     fileStringArray = fileStringLines[i].split(sp);
-    fileStringArray.pop();
-    console.log(`fileStringArray: ${fileStringArray}`)
-    fileNumberArray = []
-    for (let j = 0; j < fileStringArray.length; j++) {
-        fileNumberArray[j] = Number(fileStringArray[j]); // convert to array of numbers
-    }
+    fileNumberArray = [Number(fileStringArray[0]), Number(fileStringArray[1])];
     inputEdgeArray.push(fileNumberArray);
 };
 let originalLength = inputEdgeArray.length;
 let maxVertex = inputEdgeArray[originalLength - 1][0];  // assuming a continuous increasing set of array first elements
 
-
-// convert to adjacency lists
-
+// Destructure to reverse edges
+for (let i = 0; i < originalLength; i++)    {
+    inputEdgeArrayRev[i] = [0, 0];
+}
+for (let i = 0; i < originalLength; i++)    {
+    [inputEdgeArrayRev[i][0], inputEdgeArrayRev[i][1]] = [inputEdgeArray[i][1], inputEdgeArray[i][0]];
+}
+// convert to adjacency list
 let adjacencyList = [[1, 0]];
 let adjacencyListRev = [[1, 0]];
 // build adjacencyList template
@@ -36,15 +36,13 @@ for (let i = 0; i < maxVertex; i++) {
 for (let i = 0; i < maxVertex; i++) {
     adjacencyListRev[i] = [i + 1, 0];
 }
-// iterate through inputEdgeArray
+// Build adjacency list from inputEdgeArrayRev
 for (let i = 0; i < originalLength; i++) {
-    // reverse edges and interate through inputEdgeArrray
-    [inputEdgeArray[i][0], inputEdgeArray[i][1]] = [inputEdgeArray[i][1], inputEdgeArray[i][0]] // Destructure to reverse edges
-    inputVertex = inputEdgeArray[i][0];   // vertex number
+    inputVertex = inputEdgeArrayRev[i][0];   // vertex number
     if (adjacencyListRev[inputVertex - 1][1] == 0) {        // overwrite intial state
-        adjacencyListRev[inputVertex - 1][1] = inputEdgeArray[i][1];
+        adjacencyListRev[inputVertex - 1][1] = inputEdgeArrayRev[i][1];
     } else {
-        adjacencyListRev[inputVertex - 1].push(inputEdgeArray[i][1]);  // add element to array
+        adjacencyListRev[inputVertex - 1].push(inputEdgeArrayRev[i][1]);  // add element to array
     }
 }
 
@@ -82,7 +80,6 @@ for (let i = maxVertex; i > 0; i--) {
     DFS(adjacencyListRev, i);
 }
 console.log(`finishing times: ${finishingTimes}`);
-console.log(adjacencyListRev);
 
 // reset visitedVertices for second DFS
 for (let i = 0; i < maxVertex; i++) {
@@ -99,28 +96,24 @@ for (let i = 0; i < maxVertex; i++) {
 //     DFS2(adjacencyListRev, i);
 // };
 
-// remap edge array to finishing times ---------------------------
+// remap edge array to finishing times
 originalLength = inputEdgeArray.length;
 maxVertex = inputEdgeArray[originalLength - 1][0];
-
-console.log(inputEdgeArray + "\n");
 for (let x = 0; x < originalLength; x++)    {
-    inputEdgeArray[x][0] = finishingTimes[inputEdgeArray[x][0]];
-    inputEdgeArray[x][1] = finishingTimes[inputEdgeArray[x][1]];
+    inputEdgeArray[x][0] = finishingTimes[inputEdgeArray[x][0] - 1];
+    inputEdgeArray[x][1] = finishingTimes[inputEdgeArray[x][1] - 1];
 }
-console.log(inputEdgeArray + "\n");
 
-// iterate through inputEdgeArray in original edge order   ----   use translated edgeArray  ------
-// for (let i = 0; i < originalLength; i++) {
-//     inputVertex = inputEdgeArray[i][0];   // vertex number
-//     if (adjacencyList[inputVertex - 1][1] == 0) {        // overwrite intial state
-//         adjacencyList[inputVertex - 1][1] = inputEdgeArray[i][1];
-//     } else {
-//         adjacencyList[inputVertex - 1].push(inputEdgeArray[i][1]);  // add element to array
-//     }
-// }
-
-
+// Rebuild adjacency list using remapped edgeArray
+for (let i = 0; i < originalLength; i++) {
+    inputVertex = inputEdgeArray[i][0];   // vertex number
+    if (adjacencyList[inputVertex - 1][1] == 0) {        // overwrite intial state
+        adjacencyList[inputVertex - 1][1] = inputEdgeArray[i][1];
+    } else {
+        adjacencyList[inputVertex - 1].push(inputEdgeArray[i][1]);  // add element to array
+    }
+}
+console.log(adjacencyList);
 var connectedNodes = new Array;
 function DFS(adjacencyListRev, startVertex) {
     let startVertexIndex = startVertex - 1;
