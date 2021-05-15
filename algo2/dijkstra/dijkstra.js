720d1fb911f11cfc8099ce2247fdafe720427f08
Les Stuck <les@lesstuck.com>
2021-05-15 12:42:37 -0600

    Fix greedy algorithm not continuing at end

--- algo2/dijkstra/dijkstra.js
+++ algo2/dijkstra/dijkstra.js
@@ -27,6 +27,8 @@ for (let i = 0; i < fileStringLinesLength; i++) {
 fileNumberArray = fileStringArray;
 fileNumberArrayLength = fileNumberArray.length;
 
+//
+//
 // Dijkstra setup
 let X = new Array; // vertices processed so far (starting at 1)
 for (let i = 0; i < fileNumberArrayLength; i++) {
@@ -36,6 +38,9 @@ let V = new Array; // directed graph with positive lengths
 for (let i = 0; i < fileNumberArrayLength; i++)    {
     V[i] = fileNumberArray[i]
 }
+let min;
+let minVertex;
+let hood;
 
 let x;
 let vertexCount = 1;
@@ -44,11 +49,32 @@ let newVertex = 1;
 let pathLength = 0;
 X[0] = 0; // new! put path from 1 to 1 in X
 
+console.log(X)
 while (vertexCount <= V.length) {
-    greedyChoice = greedyChoose(V, newVertex);
-    newVertex = greedyChoice[0];
-    pathLength = pathLength + greedyChoice[1];
+    min = 1000000;
+    minVertex = 1;
+    hood = V[newVertex - 1];
+    console.log("hood: ")
+    console.log(hood)
+    for (let i = 1; i < hood.length; i++) {
+        if (X[hood[i][0] - 1] != 1000000) {
+            console.log("if")
+            continue;
+        } else if (hood[i][1] < min) {
+            console.log("else")
+            minVertex = hood[i][0];
+            min = hood[i][1];
+        }
+    }
+    console.log("return: " + minVertex + "  " + min)
+    if (min == 1000000) {
+        vertexCount++;
+        continue;
+    }
+    newVertex = minVertex;
+    pathLength = pathLength + min;
     X[newVertex - 1] = pathLength;
+    console.log("pathLength: " + pathLength)
     vertexCount++;
 }
 
@@ -61,18 +87,3 @@ console.log(X)
 // }
 // console.log(X[out[out.length - 1]]);
 
-// greedyChoose criterion
-function greedyChoose(V, v) {
-    let min = 1000000;
-    let minVertex = 1;
-    let hood = V[v - 1];
-    for (let i = 1; i < hood.length; i++) {
-        if (X[hood[i][0] - 1] != 1000000) {
-            continue;
-        } else if (hood[i][1] < min) {
-                minVertex = hood[i][0];
-                min = hood[i][1];
-        }
-    }
-    return [minVertex, min]
-}
