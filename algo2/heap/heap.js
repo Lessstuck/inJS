eb35d6d4eb69d085225e9b4109f4a2f71469320f
Les Stuck <les@lesstuck.com>
2021-05-28 17:23:34 -0600

    Delete \r in Median.txt and reads it into a numerical array

--- algo2/heap/heap.js
+++ algo2/heap/heap.js
@@ -3,5 +3,14 @@ var fs = require("fs");
 // read text file, convert to array of arrays of integers
 let fileText = fs.readFileSync('Median.txt');
 let fileString = String(fileText);
-fileStringLines = fileString.split(/\n/);
+fileStringLines = fileString.split(/\r\n/);
 fileStringLinesLength = fileStringLines.length - 1;
+for (let i = 0; i < fileStringLinesLength; i++) {
+    fileStringLines[i] = Number(fileStringLines[i])
+}
+let streamArray = fileStringLines;
+let streamArrayLength = streamArray.length;
+
+console.log(streamArrayLength)
+console.log(streamArray[0])
+console.log(streamArray[9999])
