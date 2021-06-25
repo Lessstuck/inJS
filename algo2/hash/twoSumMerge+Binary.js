7a28197298372291882433b7d2f76da8627086e7
Les Stuck <les@lesstuck.com>
2021-06-25 14:48:17 -0600

    Clean up and archive

--- algo2/hash/twoSumMerge+Binary.js
+++ algo2/hash/twoSumMerge+Binary.js
@@ -1,3 +1,6 @@
+// Correct answer = 427
+// Runtime on old MacBook = 1h20m
+
 var fs = require("fs");
 
 // read text file, convert to array of arrays of integers
@@ -21,14 +24,8 @@ console.log(Date());
 let streamArray = mergeSort(fileStringLines);
 let streamArrayLength = streamArray.length;
 
-
 for (let t = -10000; t <= 10000; t++)   {
-// for (let t = 3; t <= 10; t++)    {
-// for (let t = 10; t < 11; t++) {
     found = 0;
-    if (t % 100 == 0) {
-        console.log("t: " + t)
-    }
     for (let u = 0; u < streamArrayLength; u++)  {
         complement = t - streamArray[u];
         if (complement == streamArray[u]) {
@@ -38,16 +35,13 @@ for (let t = -10000; t <= 10000; t++)   {
         end = streamArrayLength - 1;
         if (binarySearch(streamArray, complement, start, end)) {
             found = 1;
-            // console.log("t: " + t + " streamArray[u]: " + streamArray[u] + " complement: " + complement + " count: " + count)
             continue;
         }
-        // console.log("t: " + t  + " streamArray[u]: " + streamArray[u] + " complement: " + complement)
     }
     if (found == 1) {
         count++;
     }
 }
-console.log(Date());
 console.log(count);
 
 ///////////////////////////////////////////////////// fun
@@ -95,7 +89,6 @@ function merge(left, right) {
 
 // binary search sourced from geeksforgeeks.org
 function binarySearch(arr, x, start, end) {
-    // console.log("x: " + x + " start: " + start + " end:" + end);
     if (start > end) return false;
     let mid = Math.floor((start + end) / 2);
     if (arr[mid] === x) {
